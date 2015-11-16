f02024b7bdc2b9c020ff492a2aa85d2b192ff124
Brad Burch <bradburch77@gmail.com>
2015-11-16 18:39:51 -0500

    Changed a bunch of stuff! :)

--- src/application/FXMLAccountController.java
+++ src/application/FXMLAccountController.java
@@ -1,5 +1,11 @@
 package application;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -9,23 +15,11 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.control.cell.ComboBoxListCell;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
 
 
 public class FXMLAccountController {
@@ -137,5 +131,7 @@ public class FXMLAccountController {
     	     }
     	      } 
     	   } 
-    	} 
+    	}
+    
+    
 }
