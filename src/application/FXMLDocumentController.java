406d35232b6ef19d6eb8f6aaf372581237fbb15f
Fabian Herrera <fabianherrera_2016@depauw.edu>
2015-11-16 17:11:10 -0500

    FIXED A LOT.

--- src/application/FXMLDocumentController.java
+++ src/application/FXMLDocumentController.java
@@ -98,6 +98,7 @@ public class FXMLDocumentController {
                 Scene home_page_scene = new Scene(home_page_parent);
                 FXMLAccountController controller = (FXMLAccountController)loader.getController();
                 controller.setUserName(userName);
+                controller.setPassword(passwordName);
                 controller.setUserPage();
                 //controller.setListView(sites);
                  
